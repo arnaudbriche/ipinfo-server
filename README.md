f50403bcb306be894a0b2a67aee39ed3b11f01a7
Arnaud Briche <briche.arnaud@gmail.com>
2024-02-05 16:18:30 +0100

    chore: add DNS resolution handler

--- README.md
+++ README.md
@@ -20,6 +20,14 @@ docker run -p 8081:8080 -it arnaudbriche/ipinfo-server
 
 ## Usage
 
-```
+### Getting egress IP info
+
+```sh
 curl -v 'http://localhost:8081'
+```
+
+### Resolving hostname
+
+```sh
+curl -v 'http://localhost:8080/resolve?hostname=google.com'
 ```
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"log/slog"
+	"net"
 	"net/http"
 	"os"
 )
+
+var logger = slog.New(slog.NewTextHandler(os.Stderr, nil))
 
 func main() {
 	var bindAddr = ":8080"
@@ -13,17 +17,23 @@ func main() {
 		bindAddr = os.Getenv("BIND_ADDR")
 	}
 
+	var mux = http.NewServeMux()
+	mux.HandleFunc("/", ipInfoHandler)
+	mux.HandleFunc("/resolve", resolveHandler)
+
 	var server = &http.Server{
 		Addr:    bindAddr,
-		Handler: http.HandlerFunc(handler),
+		Handler: mux,
 	}
+
+	logger.Info("HTTP server running", "addr", bindAddr)
 
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func ipInfoHandler(w http.ResponseWriter, r *http.Request) {
 	var client http.Client
 
 	req, err := http.NewRequest("GET", "https://ipinfo.io", nil)
@@ -50,6 +60,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := json.NewEncoder(w).Encode(apiResp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func resolveHandler(w http.ResponseWriter, r *http.Request) {
+	var hostname = r.URL.Query().Get("hostname")
+
+	if len(hostname) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	ips, err := net.LookupIP(hostname)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if err := json.NewEncoder(w).Encode(ips); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
